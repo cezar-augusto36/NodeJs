376fad2b403731ffe1ab37c7a1393cfc5dc1c654
Cézar Augusto <cezarograde36@gmail.com>
2021-09-06 23:56:02 -0300

    Função de Cadastrar Usuario

---
+++ acessomysql.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+
+//Conexao com MYSQL
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'celkeone',
+    password : '123456',
+    database : 'celke'
+  });
+
+  connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+   
+    console.log('connected as id ' + connection.threadId);
+  });
+
+
+connection.query('SELECT * FROM users', function(err,rows,fields){
+    if(!err){
+        console.log("Resultado: ",rows);
+    }else{
+        console.log("Erro ao realizar a consulta");
+    }
+})
+
+app.get("/", function(req, res){
+        res.sendFile(__dirname + "/src/index.html"); //__dirname serve para puxar todo o caminho do diretorio
+})
+
+app.get("/contato", function(req, res){
+    res.send("Página de Contato")
+})
+
+app.get("/sobre-empresa", function(req, res){
+    res.sendFile(__dirname+"/src/sobre-empresa.html");
+})
+
+app.get("/Blog", function(req, res){
+    res.send("Blog")
+})
+//localhost:8080
+app.listen(8080);
--- app.js
+++ app.js
@@ -11,40 +11,15 @@ const connection = mysql.createConnection({
     database : 'celke'
   });
 
-  connection.connect(function(err) {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
-    }
-   
-    console.log('connected as id ' + connection.threadId);
-  });
+connection.connect(function(err){
+    if(err) console.error('Erro ao realizar a conexão com o BD: ' + err.stack); return;
+});
 
-
-connection.query('SELECT * FROM users', function(err,rows,fields){
-    if(!err){
-        console.log("Resultado: ",rows);
-    }else{
-        console.log("Erro ao realizar a consulta");
-    }
-})
-
-app.get("/", function(req, res){
-        res.sendFile(__dirname + "/src/index.html"); //__dirname serve para puxar todo o caminho do diretorio
-})
-
-app.get("/contato", function(req, res){
-    res.send("Página de Contato")
-})
-
-app.get("/sobre-empresa", function(req, res){
-    res.sendFile(__dirname+"/src/sobre-empresa.html");
-})
-
-app.get("/Blog", function(req, res){
-    res.send("Blog")
-})
-//localhost:8080
-app.listen(8080);
-
+connection.query("INSERT INTO users(nome, email) VALUES ('Jose','jose@celke.com.br')",function(err,result){
+if(!err){
+    console.log('Usuario cadastrado com sucesso');
+}else{
+    console.log('Erro ao cadastrar Usuario');
+}
+});
 
