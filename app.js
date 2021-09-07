d42024b6b7a646dfaeecf15893e909fa2ed9fef3
Cézar Augusto <cezarograde36@gmail.com>
2021-09-06 23:40:20 -0300

    Acesso ao banco de dados MYSQL

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
 const express = require("express");
 const app = express();
 
+//Conexao com MYSQL
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'celkeone',
+    password : '123456',
+    database : 'celke'
+  });
+
+  connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+   
+    console.log('connected as id ' + connection.threadId);
+  });
+
+
+connection.query('SELECT * FROM users', function(err,rows,fields){
+    if(!err){
+        console.log("Resultado: ",rows);
+    }else{
+        console.log("Erro ao realizar a consulta");
+    }
+})
+
 app.get("/", function(req, res){
         res.sendFile(__dirname + "/src/index.html"); //__dirname serve para puxar todo o caminho do diretorio
 })
